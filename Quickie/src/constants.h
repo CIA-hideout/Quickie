7d6a3b9338fa7b0b1f6a2b949786fc45f153a2f8
Mono <monoshiro@hotmail.com>
2017-01-22 16:35:37 +0800

    fixed linker error

--- Quickie/src/constants.h
+++ Quickie/src/constants.h
@@ -1,7 +1,5 @@
 #pragma once
 
-
-
 #define PI	3.14159265
 
 #include <windows.h>
@@ -9,6 +7,23 @@
 #include <mmsystem.h>
 #include <string>
 #include <vector>
+
+#include <d3d9.h>
+#include <d3dx9.h>
+
+#pragma comment(lib, "d3d9.lib")
+#pragma comment(lib, "d3dx9.lib")
+
+// color RGB for D3DXVECTOR3
+#define COLOR_RED D3DXVECTOR3(255, 0, 0)
+#define COLOR_YELLOW D3DXVECTOR3(255, 255, 0)
+#define COLOR_PURPLE D3DXVECTOR3(255, 0, 255)
+#define COLOR_GREEN D3DXVECTOR3(0, 255, 0)
+#define COLOR_BLUE D3DXVECTOR3(0, 0, 255)
+#define COLOR_WHITE D3DXVECTOR3(255, 255, 255)
+#define DIMENSTION_SMALL D3DXVECTOR3(7, 1, 2.5)
+#define DIMENSTION_MEDIUM D3DXVECTOR3(10, 1, 2.5)
+
 
 //-----------------------------------------------
 // Useful macros
